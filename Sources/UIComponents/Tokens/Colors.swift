748312ba863fd43ba5e5d266ab66f87a66f48e6a
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2023-09-19 17:58:15 -0300

    convert extension to enum

--- Sources/UIComponents/Tokens/Colors.swift
+++ Sources/UIComponents/Tokens/Colors.swift
@@ -8,16 +8,23 @@
 import Foundation
 import SwiftUI
 
-public extension Color {
-    /// Namespace to prevent naming collisions with static accessors on
-    /// SwiftUI's Color.
-    ///
-    /// Xcode's autocomplete allows for easy discovery of design system colors.
-    /// At any call site that requires a color, type `Color.DesignSystem.<esc>`
-    public struct DesignSystem {
-        public static let brandPrimary = Color(red: 0.08627451211214066, green: 0.13333334028720856, blue: 0.545098066329956, opacity: 1)
-        public static let brandSecondary = Color(red: 1, green: 0.13333334028720856, blue: 0.3607843220233917, opacity: 1)
-        public static let neutralLightPure = Color(red: 1, green: 1, blue: 1, opacity: 1)
-        public static let neutralDarkPure = Color(red: 0, green: 0, blue: 0, opacity: 1)
+public enum AppColors {
+    case brandPrimary
+    case brandSecondary
+    case neutralLightPure
+    case neutralDarkPure
+    
+    var color: Color {
+        switch self {
+        case .brandPrimary:
+            return Color(red: 0.08627451211214066, green: 0.13333334028720856, blue: 0.545098066329956, opacity: 1)
+        case .brandSecondary:
+            return Color(red: 1, green: 0.13333334028720856, blue: 0.3607843220233917, opacity: 1)
+        case .neutralLightPure:
+            return Color(red: 1, green: 1, blue: 1, opacity: 1)
+        case .neutralDarkPure:
+            return Color(red: 0, green: 0, blue: 0, opacity: 1)
+        }
     }
 }
+
--- Sources/UIComponents/Tokens/Fonts.swift
+++ Sources/UIComponents/Tokens/Fonts.swift
@@ -6,21 +6,30 @@
 //
 
 import Foundation
-
 import SwiftUI
 
-public extension Font {
-    /// Namespace to prevent naming collisions with static accessors on
-    /// SwiftUI's Font.
-    ///
-    /// Xcode's autocomplete allows for easy discovery of design system fonts.
-    /// At any call site that requires a font, type `Font.DesignSystem.<esc>`
-    public struct DesignSystem {
-        public static let largeTitle = Font.system(size: 34, weight: .bold, design: .default)
-        public static let mediumBold = Font.system(size: 24, weight: .light, design: .default)
-        public static let normalBold = Font.system(size: 16, weight: .bold, design: .default)
-        public static let normalRegular = Font.system(size: 16, weight: .regular, design: .default)
-        public static let smallRegular = Font.system(size: 12, weight: .regular, design: .default)
-        public static let smallBold = Font.system(size: 12, weight: .bold, design: .default)
+public enum AppFonts {
+    case largeTitle
+    case mediumBold
+    case normalBold
+    case normalRegular
+    case smallRegular
+    case smallBold
+    
+    var font: Font {
+        switch self {
+        case .largeTitle:
+            return Font.system(size: 34, weight: .bold, design: .default)
+        case .mediumBold:
+            return Font.system(size: 24, weight: .light, design: .default)
+        case .normalBold:
+            return Font.system(size: 16, weight: .bold, design: .default)
+        case .normalRegular:
+            return Font.system(size: 16, weight: .regular, design: .default)
+        case .smallRegular:
+            return Font.system(size: 12, weight: .regular, design: .default)
+        case .smallBold:
+            return Font.system(size: 12, weight: .bold, design: .default)
+        }
     }
 }
